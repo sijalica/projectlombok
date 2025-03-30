c10b278937091dd4a236e29e88d7108336fc6a4f
sijalica <igorradivojevic996@gmail.com>
2025-03-30 12:33:42 +0200

    fixed init

---
+++ src/main/java/com/spring/projectlombok/ProjectLombokApplication.java
@@ -0,0 +1,13 @@
+package com.spring.projectlombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/projectlombok/ProjectLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.projectlombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
